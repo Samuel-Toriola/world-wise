3d3f8c56a8ba920f16d536c8ed9e752ebb4f62d6
Samuel-Toriola <samueloladipupo.toriola@gmail.com>
2023-12-01 14:58:05 +0100

    Initial Commit

---
+++ src/components/Map.jsx
@@ -0,0 +1,26 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+function Map() {
+  const navigate = useNavigate();
+  const [searchParamas, setSearchParams] = useSearchParams();
+  const lat = searchParamas.get("lat");
+  const lng = searchParamas.get("lng");
+  return (
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
+      <h1>Map</h1>
+      <h1>
+        Position: {lat} , {lng}
+      </h1>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+        Change position
+      </button>
+    </div>
+  );
+}
+
+export default Map;
